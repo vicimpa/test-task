9ba18d1570f32829d544c39fea1f45748679fc67
PromiSe#### <vicimpa@ya.ru>
2023-12-12 16:01:43 +0700

    Final config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,21 @@ import paths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
+  root: './src',
+  base: './',
+  build: {
+    outDir: '../dist'
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000
+  },
+  preview: {
+    host: '127.0.0.1',
+    port: 3000
+  },
   plugins: [
-    react(),
-    paths()
+    react({ plugins: [] }),
+    paths({ projects: ['../tsconfig.json'] })
   ],
 });
